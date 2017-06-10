46c662390bf7f2c338b64786f445dae7f26f7f04
Molly O'Connor <oconnormollyc@gmail.com>
2017-06-09 20:29:17 -0700

    Keepin' namin' things

--- UAV-Project/main.m
+++ UAV-Project/main.m
@@ -25,6 +25,7 @@ clr = [1 0 0; 0 0 1; 0.67 0 1; 0 1 0; 1 0.5 0];
 %% Calling the different algorithms
 [opt_rte_1, min_dist_1, smd_1, dist_history_1] =  mtsp_1(xy,dmat,salesmen,min_tour,max_tour,tw,pop_size,num_iter,use_complex,show_prog,show_res);
 [opt_rte_2, min_dist_2, smd_2, dist_history_2,total_dist, cost] =  mtsp_3(xy,dmat,salesmen,min_tour,max_tour,tw,pop_size,num_iter,use_complex,show_prog,show_res);
+[opt_rte_tnt, min_dist_tnt, smd_tnt, dist_history_tnt] =  mtsp_tnt(xy,dmat,salesmen,min_tour,max_tour,tw,pop_size,num_iter,use_complex,show_prog,show_res);
 
 %% Calculating Time/Max Distance traveled by salesman
 time(1) = max(smd_1);
